96bb0df8b569fd5311ff45be220c18addb5064e2
Jon-117 <38222965+Jon-117@users.noreply.github.com>
2024-04-07 20:03:35 -0500

    place_details view edited for POST vs GET requests

--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
-from .forms import NewPlaceForm
+from .forms import NewPlaceForm, TripReviewForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
+from django.contrib import messages
 
 
 @login_required
@@ -16,6 +17,8 @@ def place_list(request):  # must have 1 arg: request
         if form.is_valid():  # Validation against DB constraints
             place.save()     # Saves place to DB
             return redirect('place_list')  # Reloads home page (which we've named 'place_list')
+
+
 
     places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name')
     # places = Place.objects.all()
@@ -31,7 +34,7 @@ def about(request):
 
 @login_required
 def visited(request):
-    visited = Place.objects.filter(visited=True).order_by('name')
+    visited = Place.objects.filter(user=request.user).filter(visited=True).order_by('name')
     return render(request, 'travel_wishlist/visited.html', {'visited': visited})
 
 
@@ -50,7 +53,31 @@ def place_was_visited(request, place_pk):
 @login_required
 def place_details(request, place_pk):
     place = get_object_or_404(Place, pk=place_pk)
-    return render(request, 'travel_wishlist/place_detail.html', {'place': place})
+    if place.user != request.user:
+        return HttpResponseForbidden()
+        # render(request, ')travel_wishlist/place_detail.html', {'place': place})
+
+    # Does it belong to current user?
+
+    # is this a GET request (show data and form), or a POST request (update place object)
+    # if POST, validate form data & update.
+    if request.method == 'POST':
+        form = TripReviewForm(request.POST, request.FILES, instance=place)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Trip information has been updated.')
+        else:
+            messages.error(request, form.errors) # TODO - Refine later
+        return redirect('place_details', place_pk=place.pk)
+
+    else:
+        # if GET, show place & form
+        # if place is visited show form, else no form shown.
+        if place.visited:
+            reveiw_form = TripReviewForm(instance=place)
+            return render(request, 'travel_wishlist/place_details.html', {'place':place, 'reveiw_form': reveiw_form})
+        else:
+            return render(request, 'travel_wishlist/place_details.html', {'place':place})
 
 
 @login_required
